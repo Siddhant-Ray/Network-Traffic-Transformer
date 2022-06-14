702a2fe985f9f98c10e85c5829ae753d819698ff
Siddhant Ray <siddhant.r98@gmail.com>
2022-06-14 22:32:38 +0200

    plotting code updated

--- workspace/TransformerModels/plot_losses.py
+++ workspace/TransformerModels/plot_losses.py
@@ -1,4 +1,6 @@
 from tbparse import SummaryReader
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 log_dir = "../../logs/encoder_delay_logs/"
 reader = SummaryReader(log_dir)
@@ -15,3 +17,61 @@ print(train_loss_step_df)
 val_loss_step_df = df[df["tag"] == "Val loss"]
 val_loss_step_df.reset_index(inplace=True, drop=True)
 print(val_loss_step_df)
+
+## Train loss plot (pre-training)
+plt.figure(figsize=(5, 5))
+sns.lineplot(x=epoch_train_loss_df.index, y="value", data=epoch_train_loss_df, label="Avg train loss per epoch")
+plt.show()
+
+## Val loss plot (pre-training)
+plt.figure(figsize=(5, 5))
+sns.lineplot(x=val_loss_step_df.index, y="value", data=val_loss_step_df, label="Avg val loss per epoch")
+plt.show()
+
+mct_log_dir_pretrained = "../../logs/finetune_mct_logs/"
+reader_pretrained = SummaryReader(mct_log_dir_pretrained)
+df_pretrained = reader_pretrained.scalars
+
+train_loss_epoch_df_pretrained = df_pretrained[df_pretrained["tag"] == "Avg loss per epoch"]
+train_loss_epoch_df_pretrained.reset_index(inplace=True, drop=True)
+
+val_loss_epoch_df_pretrained = df_pretrained[df_pretrained["tag"] == "Val loss"]
+val_loss_epoch_df_pretrained.reset_index(inplace=True, drop=True)
+
+## Train loss (pre-trained)
+plt.figure(figsize=(5, 5))
+sns.lineplot(x=train_loss_epoch_df_pretrained.index, y="value", data=train_loss_epoch_df_pretrained, label="Avg train loss per epoch")
+plt.title("Train loss on MCT prediction (pre-trained)")
+plt.show()
+
+## Val loss (pre-trained)
+plt.figure(figsize=(5, 5))
+sns.lineplot(x=val_loss_epoch_df_pretrained.index, y="value", data=val_loss_epoch_df_pretrained, label="Avg val loss per epoch")
+plt.title("Val loss on MCT prediction (pre-trained)")
+plt.show()
+
+mct_log_dir_nonpretrained = "../../logs/finetune_mct_logs2/"
+reader_nonpretrained = SummaryReader(mct_log_dir_nonpretrained)
+df_nonpretrained = reader_nonpretrained.scalars
+
+train_loss_epoch_df_nonpretrained = df_nonpretrained[df_nonpretrained["tag"] == "Avg loss per epoch"]
+train_loss_epoch_df_nonpretrained.reset_index(inplace=True, drop=True)
+
+val_loss_epoch_df_nonpretrained = df_nonpretrained[df_nonpretrained["tag"] == "Val loss"]
+val_loss_epoch_df_nonpretrained.reset_index(inplace=True, drop=True)
+
+## Train loss (non-pretrained)
+plt.figure(figsize=(5, 5))
+sns.lineplot(x=train_loss_epoch_df_nonpretrained.index, y="value", data=train_loss_epoch_df_nonpretrained, label="Avg train loss per epoch")
+plt.title("Train loss on MCT prediction (non-pretrained)")
+plt.show()
+
+## Val loss (non-pretrained)
+plt.figure(figsize=(5, 5))
+sns.lineplot(x=val_loss_epoch_df_nonpretrained.index, y="value", data=val_loss_epoch_df_nonpretrained, label="Avg val loss per epoch")
+plt.title("Val loss on MCT prediction (non-pretrained)")
+plt.show()
+
+
+
+
--- workspace/TransformerModels/plot_predictions.py
+++ workspace/TransformerModels/plot_predictions.py
@@ -23,26 +23,49 @@ def main():
     actual_values = np.load(path+"actual_last_delay_window_size_{}.npy".format(window_size))
     transformer_predictions = np.load(path+"transformer_last_delay_window_size_{}.npy".format(window_size))
     arma_predictions = np.load(path+"arma_last_delay_window_size_{}.npy".format(window_size))
-
+    penultimate_predictions = np.load(path+"penultimate_last_delay_window_size_{}.npy".format(window_size))
+    ewm_predictions = np.load(path+"ewm_delay_window_size_{}.npy".format(window_size))
+    
     assert (actual_values.shape == transformer_predictions.shape == arma_predictions.shape)
+    assert (actual_values.shape == penultimate_predictions.shape == ewm_predictions.shape)
 
     transformer_squared_losses = np.square(np.subtract(transformer_predictions, actual_values))
     arma_squared_losses = np.square(np.subtract(arma_predictions, actual_values))
+    penultimate_squared_losses = np.square(np.subtract(penultimate_predictions, actual_values))
+    ewm_squared_losses = np.square(np.subtract(ewm_predictions, actual_values))
 
     assert(transformer_squared_losses.shape == arma_squared_losses.shape)
 
     print("Mean loss on last delay from transformer is : ", np.mean(np.array(transformer_squared_losses)))
-    print("Mean loss on last delay from arma is : ", np.mean(np.array(arma_squared_losses)))
-
     print("99%%ile loss on last delay from transformer is : ", np.percentile(np.array(transformer_squared_losses), 99))
+
+    print("Mean loss on last delay from arma is : ", np.mean(np.array(arma_squared_losses)))
     print("99%%ile loss on last delay from arma is : ", np.percentile(np.array(arma_squared_losses), 99))
+
+    print("Mean loss on last delay from penultimate is : ", np.mean(np.array(penultimate_squared_losses)))
+    print("99%%ile loss on last delay from penultimate is : ", np.percentile(np.array(penultimate_squared_losses), 99))
+
+    print("Mean loss on last delay from ewma is : ", np.mean(np.array(ewm_squared_losses)))
+    print("99%%ile loss on last delay from ewma is : ", np.percentile(np.array(ewm_squared_losses), 99))
+
+    if args.path == "plot_values":
+        median_predictions = np.load(path+"median_last_delay_window_size_{}.npy".format(window_size))
+        assert(actual_values.shape == median_predictions.shape)
+
+        median_squared_losses = np.square(np.subtract(median_predictions, actual_values))
+
+        print("Mean loss on last delay from median is : ", np.mean(np.array(median_squared_losses)))
+        print("99%%ile loss on last delay from median is : ", np.percentile(np.array(median_squared_losses), 99))
+
+    ## Save path 
+    save_path = "plots/" + path 
 
     # Plot the actual delay 
     plt.figure()
     sns.histplot(actual_values)
     plt.legend([],[], frameon=False)
     plt.title("Actual delay distribution")
-    plt.savefig("plots/actual_last_delay.png")
+    plt.savefig(save_path + "actual_last_delay.png")
 
     # Plot the transformer predictions
     plt.figure()
@@ -50,14 +73,14 @@ def main():
     plt.legend([],[], frameon=False)
     plt.title("Transformer predicted delay distribution")
 
-    plt.savefig("plots/transformer_predictions.png")
+    plt.savefig(save_path + "transformer_predictions.png")
 
     # Plot the arma predictions
     plt.figure()
     sns.histplot(arma_predictions)
     plt.legend([],[], frameon=False)
     plt.title("ARMA predicted delay distribution")
-    plt.savefig("plots/arma_predictions.png")
+    plt.savefig(save_path + "arma_predictions.png")
 
     # Plot histograms of transformer and arma squared losses
     plt.figure()
@@ -65,13 +88,16 @@ def main():
     ax.set(xlabel='Transformer Losses', ylabel='Frequency')
     plt.legend([],[], frameon=False)
     plt.title("Squared loss from transformer vs actual distribution")
-    plt.savefig("plots/histogram_squared_losses_transformer.png")
+    plt.xlim(0, 0.0005)
+    plt.savefig(save_path + "histogram_squared_losses_transformer.png")
+
     plt.figure()
     ax=sns.histplot(arma_squared_losses, color="red")
     ax.set(xlabel='ARMA Losses', ylabel='Frequency')
     plt.legend([],[], frameon=False)
     plt.title("Squared loss from ARMA vs actual distribution")
-    plt.savefig("plots/histogram_squared_losses_arma.png")
+    plt.xlim(0, 0.0005)
+    plt.savefig(save_path + "histogram_squared_losses_arma.png")
     
 if __name__=="__main__":
     main()
